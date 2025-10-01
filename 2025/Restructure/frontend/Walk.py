46ca548d0a858edc32df16352c608570ab108830
Lochie007 <a1826222@adelaide.edu.au>
2025-10-01 21:40:59 +0930

    Bones in for WalkGait (Walk.py)

--- 2025/Restructure/frontend/Walk.py
+++ 2025/Restructure/frontend/Walk.py
@@ -10,23 +10,34 @@ class WalkGait:
         self.rb = OpenRB  # serial comms
         self.angles = Angles
         incrementer = 30 #mm
+        sticky = True
 
     # Automatically appends EE
         # Takes in a leg number, and a 'sticky' flag
         # Returns a Bool
             # True if sensors give good reading after EE current reached
             # False if sensors give bad reading after EE current reached
-    def appedEE()
+    def appedEE(legNum, stickyFlag)
+        # Checking for sticky flag
+        if stickyFlag
+            return True
+        
         # Start adhering EE
+        while current < maxCurrent
+            # send sync write to activate leg ee
+            # run a timer to determine how long it moves for
 
         # Once current is reached, run if statement
             # if all sensors are green
-                # return True
+        if sensorVoltage > 13.2 #Assuming sensor voltage of 4.4V per sensor
+            return True
             
             # if less than 3 sensors are green 
-                # retract EE
-                # return false when fully retracted
-
+        if sensorVoltage <= 13.2
+            # send sync write to deactivate leg ee, runnning for a duration as determined by the above timer
+            
+            # one ee full disengaged
+            return False
 
 
 
@@ -39,27 +50,31 @@ class WalkGait:
         # Returns a Bool
             # True if successful
             # False if failure occurs
-    def moveLimb(legNum)
+    def moveLimb(legNum, goalPos)
         # Read xyz of requested EE
-        legXYZ = Positions.get_all_leg_positions().[legNum]
+        legXYZ = Positions.get_all_leg_positions().[legNum] # This is fro Position.py
 
         # Translate the EE +z 
         legXYZ[2] += incrementer
-        sol, feasible = self._solve_leg_for_world_target(self.leg, self.Pw)
-            if feasible:
-                self.last_valid[self.leg]["sol"] = sol
-                self.last_valid[self.leg]["Pw"]  = self.Pw.copy()
-            else:
-                sol = self.last_valid[self.leg]["sol"] if self.last_valid[self.leg]["sol"] else sol
-                if self.last_valid[self.leg]["Pw"] is not None:
-                    self.Pw = self.last_valid[self.leg]["Pw"].copy()
-
-
+        sol, feasible = self._solve_leg_for_world_target(self.leg, legXYZ) # This is fro Position.py
+            # May need to include a condition to deny if its feasable
+            # send sol angles to sync write
+            
         # Move EE to requested x and y location
+        legXYZ[0] = goalPos[0] # x
+        legXYZ[1] = goalPos[1] # y
+        sol, feasible = self._solve_leg_for_world_target(self.leg, legXYZ) # This is fro Position.py
+            # May need to include a condition to deny if its feasable
+            # send sol angles to sync write
 
         # Translate the EE -z
+        sol, feasible = self._solve_leg_for_world_target(self.leg, goalPos) # This is fro Position.py
+            # May need to include a condition to deny if its feasable
+            # send sol angles to sync write
 
-        # return True
+        # NEED a way to keep track of XYZ positions
+
+       return True
 
 
 
@@ -74,34 +89,84 @@ class WalkGait:
 
         # Given the requested direction, calculate the EE positions
         if direction == 'N'
-            
-
+            leg1XYZ = [175.0, -145.26, -207.5]
+            leg2XYZ = [0.0, -145.26, -207.5]
+            leg3XYZ = [0.0, 145.26, -207.5]
+            leg4XYZ = [175.0, 145.26, -207.5]
+        elif direction == 'S'
+            leg1XYZ = [0.0, -145.26, -207.5]
+            leg2XYZ = [-175.0, -145.26, -207.5]
+            leg3XYZ = [-175.0, 145.26, -207.5]
+            leg4XYZ = [0.0, 145.26, -207.5]
+        elif direction == 'W'
+            leg1XYZ = [160.0, 0.0, -207.5]
+            leg2XYZ = [-160.0, 0.0, -207.5]
+            leg3XYZ = [-160.0, 175.0, -207.5]
+            leg4XYZ = [160.0, 175.0, -207.5]
+        elif direction == 'E'
+            leg1XYZ = [160.0, 175.0, -207.5]
+            leg2XYZ = [-160.0, 175.0, -207.5]
+            leg3XYZ = [-160.0, 0.0, -207.5]
+            leg4XYZ = [160.0, 0.0, -207.5]
     
         # Start gait cycle
-            # Move leg 1
-            # call appendEE
-                # if false, call appendEE again
-                # if true, move on
-            
-            # Move leg 2
-            # call appendEE
-                # if false, call appendEE again
-                # if true, move on
+        # Move leg 1
+        moveLimb(1, leg1XYZ)
+        # call appendEE
+        eeStatus = appedEE(1, stickyFlag) 
+            # if false, call appendEE again
+            # if true, move on
+        failureCount = 0
+        if !eeStatus
+            eeStatus = appedEE(1, stickyFlag)
+            failureCount++
+            if failureCount > 3
+                return False
 
-            # Move leg 3
-            # call appendEE
-                # if false, call appendEE again
-                # if true, move on
+        # Move leg 2
+        moveLimb(2, leg2XYZ)
+        # call appendEE
+        eeStatus = appedEE(2, stickyFlag) 
+            # if false, call appendEE again
+            # if true, move on
+        failureCount = 0
+        if !eeStatus
+            eeStatus = appedEE(2, stickyFlag)
+            failureCount++
+            if failureCount > 3
+                return False
 
-            # Move leg 4
-            # call appendEE
-                # if false, call appendEE again
-                # if true, move on
+        # Move leg 3
+        moveLimb(3, leg3XYZ)
+        # call appendEE
+        eeStatus = appedEE(3, stickyFlag) 
+            # if false, call appendEE again
+            # if true, move on
+        failureCount = 0
+        if !eeStatus
+            eeStatus = appedEE(3, stickyFlag)
+            failureCount++
+            if failureCount > 3
+                return False
+
+        # Move leg 4
+        moveLimb(4, leg4XYZ)
+        # call appendEE
+        eeStatus = appedEE(4, stickyFlag) 
+            # if false, call appendEE again
+            # if true, move on
+        failureCount = 0
+        if !eeStatus
+            eeStatus = appedEE(4, stickyFlag)
+            failureCount++
+            if failureCount > 3
+                return False
+
 
             # Move body in direction 
-
+                # maybe call a move body func from Position.py
     
-
+        return True
 
 
 
@@ -109,32 +174,68 @@ class WalkGait:
         # returns true when complete
         # returns false if failure occurs 
     def moveToInitial()
+
+
+        leg1Home = [159.0, -145.0, -205.0]
+        leg2Home = [-159.0, -145.0, -205.0]
+        leg3Home = [-159.0, 145.0, -205.0]
+        leg4Home = [159.0, 145.0, -205.0]
             
         # Move the robot back into its initial position    
-            # Move leg 1
-            # call appendEE
-                # if false, call appendEE again
-                # if true, move on
+        # Move leg 1
+        moveLimb(1, leg1Home)
+        # call appendEE
+        eeStatus = appedEE(1, stickyFlag) 
+            # if false, call appendEE again
+            # if true, move on
+        failureCount = 0
+        if !eeStatus
+            eeStatus = appedEE(1, stickyFlag)
+            failureCount++
+            if failureCount > 3
+                return False
+
+        # Move leg 2
+        moveLimb(2, leg2Home)
+        # call appendEE
+        eeStatus = appedEE(2, stickyFlag) 
+            # if false, call appendEE again
+            # if true, move on
+        failureCount = 0
+        if !eeStatus
+            eeStatus = appedEE(2, stickyFlag)
+            failureCount++
+            if failureCount > 3
+                return False
+
+        # Move leg 3
+        moveLimb(3, leg3Home)
+        # call appendEE
+        eeStatus = appedEE(3, stickyFlag) 
+            # if false, call appendEE again
+            # if true, move on
+        failureCount = 0
+        if !eeStatus
+            eeStatus = appedEE(3, stickyFlag)
+            failureCount++
+            if failureCount > 3
+                return False
+
+        # Move leg 4
+        moveLimb(4, leg4Home)
+        # call appendEE
+        eeStatus = appedEE(4, stickyFlag) 
+            # if false, call appendEE again
+            # if true, move on
+        failureCount = 0
+        if !eeStatus
+            eeStatus = appedEE(4, stickyFlag)
+            failureCount++
+            if failureCount > 3
+                return False
+
+        return True
             
-            # Move leg 2
-            # call appendEE
-                # if false, call appendEE again
-                # if true, move on
-
-            # Move leg 3
-            # call appendEE
-                # if false, call appendEE again
-                # if true, move on
-
-            # Move leg 4
-            # call appendEE
-                # if false, call appendEE again
-                # if true, move on
-
-            # Move body forward
-            
-
-
 
 
 
@@ -143,7 +244,7 @@ class WalkGait:
         sync_targets = []
 
         # Move robot into initial psoition and adhering
-
+        moveToInitial()
 
         # Reading in joystick
         js = getattr(self.controller, "joystick", None)
